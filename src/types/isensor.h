c07bcc1b2b8c307160a5440341771b54bd47329c
jpolanco <jpolanco@getinsoft.com>
2020-10-16 13:29:41 -0500

    [Fix] ISensor abstract function registerMeasure parameters

--- src/types/isensor.h
+++ src/types/isensor.h
@@ -1,12 +1,17 @@
 #if !defined(AGRICOS_SENSOR_ISENSOR_H)
 #define AGRICOS_SENSOR_ISENSOR_H
 
+#include <stdint.h>
+
+#include "variable_types.h"
+
 class ISensor
 {
 public:
     virtual void setup() = 0;
     virtual void update() = 0;
-    virtual void registerMeasure(uint8_t ) = 0;
+
+    virtual void registerMeasure() = 0;
 };
 
-#endif // AGRICOS_SENSOR_ISENSOR_H
+#endif  // AGRICOS_SENSOR_ISENSOR_H
